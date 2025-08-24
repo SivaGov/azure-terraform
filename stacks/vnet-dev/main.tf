e26b9525ee4d71b1b4b4178cabb972ce9d826739
SivaGov <Shiva.raj_2006@yahoo.com>
2025-08-23 18:47:17 -0700

    allfolders

---
+++ stacks/vnet-dev/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_version = ">= 1.7.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.118"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # Auth will come from env vars ARM_* set by the pipeline (or your local shell).
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+# Resource group (dev only)
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.name}-rg"
+  location = var.location
+  tags     = var.tags
+}
+
+# VNet
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.name}-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = [var.vnet_cidr]
+  tags                = var.tags
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.name}-subnet01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_cidr]
+}
+
+output "rg_name"     { value = azurerm_resource_group.rg.name }
+output "vnet_name"   { value = azurerm_virtual_network.vnet.name }
+output "subnet_name" { value = azurerm_subnet.subnet.name }
+
---
+++ stacks/vnet-dev/variables.tf
@@ -0,0 +1,11 @@
+variable "name"            { type = string }
+variable "location"        { type = string }
+variable "subscription_id" { type = string }
+variable "tenant_id"       { type = string }
+variable "vnet_cidr"       { type = string }
+variable "subnet_cidr"     { type = string }
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
