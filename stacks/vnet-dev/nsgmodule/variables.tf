751bce4edc56129c6f03ec19242bf7ce11e99fc2
SivaGov <Shiva.raj_2006@yahoo.com>
2025-09-14 00:06:56 -0700

    testing modules15

--- stacks/vnet-dev/nsgmodule/variables.tf
+++ stacks/vnet-dev/nsgmodule/variables.tf
@@ -1,5 +1,8 @@
 variable "name"            { type = string }
 variable "location"        { type = string }
+variable "vnet_cidr"       { type = string }
+variable "subnet_cidr"     { type = string }
+variable "subnet_cidr2"     { type = string }
 variable "tags" {
   type    = map(string)
   default = {}
